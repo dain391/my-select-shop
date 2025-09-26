cd58347c64fa6cbbb1d07417448f9f0ee6d53d14
dain <0901ekdls@gmail.com>
2025-09-26 10:02:31 +0900

    feat: 폴더별 관심 상품 조회 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -61,4 +61,24 @@ public class ProductController {
 	) {
 		productService.addFolder(productId, folderId, userDetails.getUser());
 	}
+
+	// 폴더 내 전체 상품 조회
+	@GetMapping("/folders/{folderId}/products")
+	public Page<ProductResponseDto> getProductsInFold(
+		@PathVariable Long folderId,
+		@RequestParam int page,
+		@RequestParam int size,
+		@RequestParam String sortBy,
+		@RequestParam boolean isAsc,
+		@AuthenticationPrincipal UserDetailsImpl userDetails
+	) {
+		return productService.getProductsInFolder(
+			folderId,
+			page-1,
+			size,
+			sortBy,
+			isAsc,
+			userDetails.getUser()
+		);
+	}
 }
